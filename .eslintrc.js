cd37c7a236243e82cc930bc96b50ea07cd2cbe4b
Erick Pac <erickj.pac@gmail.com>
2024-11-14 14:25:17 -0600

    chore: set up prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: "expo",
+  extends: ["expo", "prettier"],
+  plugins: ["prettier"],
   ignorePatterns: ["/dist/*"],
   rules: {
+    "prettier/prettier": "error",
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/no-unused-vars": "warn",
--- app/(tabs)/(profile)/index.tsx
+++ app/(tabs)/(profile)/index.tsx
@@ -1,7 +1,6 @@
 import { StyleSheet } from "react-native";
 import { ThemedText } from "@/components/common/themed-text";
 import { ThemedView } from "@/components/common/themed-view";
-import { useThemeColor } from "@/hooks/common/useThemeColor";
 
 export default function MoreScreen() {
   return (
