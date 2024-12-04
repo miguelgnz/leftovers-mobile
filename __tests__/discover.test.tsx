65eae0c8163f14f0821a488be064354de2cf11fd
Miguel González <mgx.gon@gmail.com>
2024-12-03 18:25:55 -0600

    update text assertion in DiscoverTab example test

--- __tests__/discover.test.tsx
+++ __tests__/discover.test.tsx
@@ -3,9 +3,9 @@ import { render } from "@testing-library/react-native";
 import DiscoverTab from "@/app/(tabs)/(discover)/index";
 
 describe("Discover Tab", () => {
-  test("Renders correctly", () => {
+  test("Text renders correctly", () => {
     const { getByText } = render(<DiscoverTab />);
 
-    getByText("Text renders correctly on Discover tab");
+    getByText("Edit app/index.tsx to edit this screen.");
   });
 });
