8461f4a6719ea310aa1da13f34042ef7b926df4f
Miguel González <mgx.gon@gmail.com>
2024-12-13 13:09:21 -0600

    integrate and setup safe area context for better layout handling

--- app/(tabs)/(discover)/index.tsx
+++ app/(tabs)/(discover)/index.tsx
@@ -1,8 +1,15 @@
 import { View, Text } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Home() {
+  const insets = useSafeAreaInsets();
+
+  console.log("Bottom inset:", insets.bottom, "Top inset:", insets.top);
   return (
-    <View className="flex-1 items-center justify-center">
+    <View
+      className={`flex-1 items-center justify-center`}
+      style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}
+    >
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,7 @@ import { useFonts } from "expo-font";
 import { useColorScheme } from "react-native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { I18nextProvider } from "react-i18next";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import i18n from "@/locales/i18n";
 import "@/global.css";
 
@@ -36,13 +37,15 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <QueryClientProvider client={queryClient}>
-        <I18nextProvider i18n={i18n}>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          </Stack>
-        </I18nextProvider>
-      </QueryClientProvider>
+      <SafeAreaProvider>
+        <QueryClientProvider client={queryClient}>
+          <I18nextProvider i18n={i18n}>
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            </Stack>
+          </I18nextProvider>
+        </QueryClientProvider>
+      </SafeAreaProvider>
     </ThemeProvider>
   );
 }
